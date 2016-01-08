8f5e63dc960aacf0d01560add6e169f42c7f6769
jnesta <j.nesta@gmail.com>
2016-01-08 09:00:21 -0500

    more customizable installation script

--- RUN THIS TO INSTALL.bat
+++ RUN THIS TO INSTALL.bat
@@ -1,30 +1,29 @@
 @echo off
 
-rem Check to see if the user is using the default resources path
-if not exist "C:\Program Files (x86)\Steam\SteamApps\common\The Binding of Isaac Rebirth\resources" (
+rem Set this equal to where your Afterbirth resources folder is
+set ResourcesFolder=C:\Program Files (x86)\Steam\SteamApps\common\The Binding of Isaac Rebirth\resources
+
+rem Check to see if we can find the resources folder
+if not exist "%ResourcesFolder%" (
 	echo.
-	echo ^+------------------------------------------------------------------------^+
-	echo ^| ERROR: I could not find the Afterbirth resources folder at:            ^|
-	echo ^|                                                                        ^|
-	echo ^|        "C:\Program Files (x86)\Steam\SteamApps\common\                 |
-	echo ^|         The Binding of Isaac Rebirth\resources\"                       |
-	echo ^|                                                                        ^|
-	echo ^|        You will have to install the mod manually by copying the        ^|
-	echo ^|        contents of the directory with the ruleset that you want to     ^|
-	echo ^|        play into the Afterbirth resources folder.                      ^|
-	echo ^+------------------------------------------------------------------------^+
+	echo ERROR: I could not find the Afterbirth resources folder at:
+	echo "%ResourcesFolder%"
+	echo.
+	echo Edit this file with Notepad and fix the path of the folder on line 4.
+	echo Or, just install the mod manually by copying contents of the directory
+	echo with the ruleset that you want to play into the Afterbirth resources folder.
 	echo.
 	pause
 	exit /B
 )
 
-rem Welcome message and prompt user for the kind of ruleset
+rem Prompt the user for the kind of ruleset to use
 echo.
-echo ^+----------------------------------------------------------^+
-echo ^| WARNING: This script will delete all current mods in     ^|
-echo ^|          your Afterbirth resources folder. If this is    ^|
-echo ^|          is not what you want, close this window now.    ^|
-echo ^+----------------------------------------------------------^+
+echo ^+-------------------------------------------------------------^+
+echo ^| WARNING: This script will delete all of the current mods    ^|
+echo ^|          in your Afterbirth resources folder. If this is    ^|
+echo ^|          is not what you want, close this window now.       ^|
+echo ^+-------------------------------------------------------------^+
 echo.
 echo Choose a ruleset:
 echo   1) Normal
@@ -55,23 +54,23 @@ rem Start the installation
 :start 
 
 rem Delete all files
-for %%i in ("C:\Program Files (x86)\Steam\SteamApps\common\The Binding of Isaac Rebirth\resources\*") do (
+for %%i in ("%ResourcesFolder%\*") do (
 	del /Q "%%i"
 )
 
 rem Delete all directories except for the resources one
-for /D %%i in ("C:\Program Files (x86)\Steam\SteamApps\common\The Binding of Isaac Rebirth\resources\*") do (
+for /D %%i in ("%ResourcesFolder%\*") do (
 	if "%%~nxi" NEQ "packed" rd /S /Q "%%i"
 )
 
 rem Copy the files over (the /S flag is to include folders and the /Q flag is to make it be quiet)
-if [%id%] == [1] xcopy /S /Q "%~dp0Ruleset 1 - Normal" "C:\Program Files (x86)\Steam\SteamApps\common\The Binding of Isaac Rebirth\resources"
-if [%id%] == [2] xcopy /S /Q "%~dp0Ruleset 2 - More Options" "C:\Program Files (x86)\Steam\SteamApps\common\The Binding of Isaac Rebirth\resources"
-if [%id%] == [3] xcopy /S /Q "%~dp0Ruleset 3 - Seeded" "C:\Program Files (x86)\Steam\SteamApps\common\The Binding of Isaac Rebirth\resources"
-if [%id%] == [4] xcopy /S /Q "%~dp0Ruleset 4 - Dark Room" "C:\Program Files (x86)\Steam\SteamApps\common\The Binding of Isaac Rebirth\resources"
-if [%id%] == [5] xcopy /S /Q "%~dp0Ruleset 5 - Seeded Dark Room" "C:\Program Files (x86)\Steam\SteamApps\common\The Binding of Isaac Rebirth\resources"
+if [%id%] == [1] xcopy /S /Q "%~dp0Ruleset 1 - Normal" "%ResourcesFolder%"
+if [%id%] == [2] xcopy /S /Q "%~dp0Ruleset 2 - More Options" "%ResourcesFolder%"
+if [%id%] == [3] xcopy /S /Q "%~dp0Ruleset 3 - Seeded" "%ResourcesFolder%"
+if [%id%] == [4] xcopy /S /Q "%~dp0Ruleset 4 - Dark Room" "%ResourcesFolder%"
+if [%id%] == [5] xcopy /S /Q "%~dp0Ruleset 5 - Seeded Dark Room" "%ResourcesFolder%"
 if [%id%] == [6] echo All mods have been uninstalled.
 
-rem Done
+rem All done
 echo.
 pause
